296cca67f57fd28a49d442a3c5f85552053cb9e2
Keyla Simi <keylasimi@outlook.com>
2020-12-06 15:17:37 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dscatalog/resource/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resource/CategoryResource.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 /** no java List é uma interface. Interface não pode ser instanciada. Deve se instanciar uma classe que implementa uma interface.**/
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
  
 /**  
  * Annotation: Essa anotação é usada para fazer uma classe como um controlador da web, 
@@ -25,12 +27,13 @@ import com.devsuperior.dscatalog.entities.Category;
  */
 @RequestMapping(value="/categories")
 public class CategoryResource {
+	
+	@Autowired
+	private CategoryService service;
 
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Books"));
-		list.add(new Category(2L, "Electronics"));
+		List<Category> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
