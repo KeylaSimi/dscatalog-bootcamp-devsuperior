63739f144ad63afecc40b68127de2aec0714086f
Keyla Simi <keylasimi@outlook.com>
2020-12-09 21:48:58 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/resource/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resource/CategoryResource.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dscatalog.resource;
 
-import java.util.ArrayList;
 import java.util.List;
 /** no java List é uma interface. Interface não pode ser instanciada. Deve se instanciar uma classe que implementa uma interface.**/
 
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
  
 /**  
@@ -32,8 +31,8 @@ public class CategoryResource {
 	private CategoryService service;
 
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -16,8 +18,23 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional (readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		// map: aplica uma função a cada elemento da coleção original, retornando uma nova coleção.
+		// filter: retorna uma nova coleção contendo apenas aqueles elementos da coleção que satisfazem um dado predicado.
+		// reduce: aplica cumulativamente uma função aos elementos de uma coleção, retornando o resultado final.
+		//         pode-se informar, opcionalmente, um valor inicial como parâmetro (necessário para uma coleção vazia).
+		// sort: ordena a coleção conforme a função de comparação informada como parâmetro.		
+				
+		
+//		List<CategoryDTO> listDto = new ArrayList<>();
+//		for (Category cat : list) {
+//			listDto.add(new CategoryDTO(cat));
+//		}
+		
+//		return listDto;
 	}
 	
 }
