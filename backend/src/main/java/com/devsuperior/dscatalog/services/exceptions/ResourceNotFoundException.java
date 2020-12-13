c67c06430fcedb3a71fdb27fbac2971d65e122e4
Keyla Simi <keylasimi@outlook.com>
2020-12-13 18:42:47 -0300

    Update category

--- backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourceNotFoundException.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourceNotFoundException.java
@@ -1,11 +1,11 @@
 package com.devsuperior.dscatalog.services.exceptions;
 
-public class EntityNotFoundException extends RuntimeException {
+public class ResourceNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	
 	//  super = é usado para repassar o argumento (msg) de um construdor 
 	//para o construtor de uma super classe.
-	public EntityNotFoundException (String msg) {
+	public ResourceNotFoundException (String msg) {
 		super(msg);
 	}
 
