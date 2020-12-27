90d4e51607e11ecac4a36809fd26d1eced69c536
Keyla Simi <keylasimi@outlook.com>
2020-12-27 16:09:46 -0300

    Auditing data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into tb_category (name) values ('BOOKS');
-insert into tb_category (name) values ('ELECTRONICS');
-insert into tb_category (name) values ('COMPUTERS');
+insert into tb_category (name, created_At) values ('BOOKS', NOW());
+insert into tb_category (name, created_At) values ('ELECTRONICS', NOW());
+insert into tb_category (name, created_At) values ('COMPUTERS', NOW());
